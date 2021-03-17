622fc5df738afd6a7867d32c8276e4a3d21feeb2
kshitizagarwal-dev <76765982+kshitizagarwal-dev@users.noreply.github.com>
2021-03-17 09:43:49 +0530

    Update TeaVendingMachine.py

--- TeaVendingMachine.py
+++ TeaVendingMachine.py
@@ -6,7 +6,7 @@ class teaVendingMachine:
     ginger_tea = 25
     black_tea = 15
     green_tea = 50
-    info = {}
+
     customer_name = ""
     lis = []
 # for initializing by using name
@@ -16,19 +16,21 @@ class teaVendingMachine:
 
 # For making tea only works for Plain and Ginger Tea
     def makeTea(self):
+        
         self.tea = int(input('enter the amount of tea leaves in tsp'))
-        self.milk = int(input('enter the amount of milk in tsp'))
+        self.milk = int(input('enter the amount of milk in cups'))
+        
         self.lis.append(self.tea)
         self.lis.append(self.milk)
+        
         if(bool(input('For sugar type True otherwise type False '))): # asking whether they required sugar or not
             self.sugar = int(input('Enter the amount of sugar in tsp'))
             self.lis.append(self.sugar)
+        return self.lis
 
 
 
-# for storing the choice of the customer
-    def store_info(self):
-         self.info[self.customer_name] = self.lis
+
 
 
 if __name__ == '__main__':
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     name = input('Enter your name')
     # creating instance of class
     obj = teaVendingMachine(name)
-
+    info = {}
      # different choices for customer
     print('press 1 for Plain Tea')
     print('press 2 for Ginger Tea')
@@ -46,14 +48,16 @@ if __name__ == '__main__':
     ch = int(input())
 
     if ch == 1:
-        obj.makeTea()
+
+        info[name] =  obj.makeTea()
         print('The tea is ready!!!')
-        obj.store_info()
+
 
     if ch == 2:
-        obj.makeTea()
+
+        info[name] =  obj.makeTea()
         print('The tea is ready!!!')
-        obj.store_info()
+
 
     if ch == 3:
         print('The tea is ready!!!')
@@ -61,6 +65,6 @@ if __name__ == '__main__':
     if ch == 4:
         print('The tea is ready!!!')
 
-    print(obj.info)
+    print(info)
 
 
